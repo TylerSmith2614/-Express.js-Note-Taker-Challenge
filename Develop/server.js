8c741b7d28efb11d2d319193440c6bb0a378a3f9
Tyler Smith <tylersmith2614@gmail.com>
2023-05-10 16:52:27 -0600

    added app.get and app.post functions

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const fs = require("fs");
+const uuidv1 = require("uuid/v1");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -19,19 +20,27 @@ app.get("/notes", (req, res) => {
 });
 
 // reads the data in the db.json file. Parses the data so it can be used on the dom (meaning viewable on the webpage)
-// app.get("/notes", (req, res) => {
-//   fs.readFile("./db/db.json", "utf-8", (err, notes) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send("Broken");
-//       return;
-//     }
-//     const jsonData = JSON.parse(notes);
-//     res.json(jsonData);
-//   });
-// });
+const filePath = path.join(__dirname, "db", "db.json");
+app.get("/api/notes", (req, res) => {
+  fs.readFile(filePath, "utf-8", (err, notes) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Broken");
+      return;
+    }
+    const jsonData = JSON.parse(notes);
+    res.json(jsonData);
+  });
+});
 
 // will have a write file as app.post()
+app.post("/api/notes", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(filePath));
+  const newNotes = { id: uuidv1(), title: req.body.title, text: req.body.text };
+  notes.push(newNotes);
+  fs.writeFileSync(filePath, JSON.stringify(notes));
+  res.json(newNotes);
+});
 
 // for the delete add uuidv1
 
